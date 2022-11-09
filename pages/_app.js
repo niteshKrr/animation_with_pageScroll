5c904f56c4ce1bea647e5db146f69441a0f1ae8b
Niteshkumar <nkumar35101@gmail.com>
2022-11-09 20:23:34 +0530

    animation with slider on page scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+import "../styles/globals.css";
+// ..
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init({
+      offset: 300,
+      duration: 1000,
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
